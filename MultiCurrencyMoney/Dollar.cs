6db9f824d1bfd5ccf1e20498ddbc3af19127d08a
naunow <naunow00@gmail.com>
2020-02-01 18:48:57 +0900

    『テスト駆動開発』 Written by Kent Beck
    CopyCoding 「他国通貨」 P.3~

---
+++ MultiCurrencyMoney/Dollar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiCurrencyMoney
+{
+    public class Dollar
+    {
+        public int amount;
+        public Dollar(int amount)
+        {
+            // コンパイルエラーを通すことが主眼なので、中身は空で良い。
+        }
+
+        public void Times(int multiplier)
+        {
+            // コンパイルエラーを通すことが主眼なので、中身は空で良い。
+        }
+    }
+}
---
+++ MultiCurrencyMoneyTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiCurrencyMoney;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiCurrencyMoney.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.Times(2);
+            Assert.AreEqual(10, five.amount);
+        }
+    }
+}
