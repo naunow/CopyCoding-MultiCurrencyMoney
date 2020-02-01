0eb53bac77821cf64fda053ada287fadd7ea99ad
naunow <naunow00@gmail.com>
2020-02-01 19:12:18 +0900

    P10 重複の除去のため、コンストラクタの中でamountフィールドを代入しメソッド内では5ではなくamountを使用する。

--- MultiCurrencyMoney/Dollar.cs
+++ MultiCurrencyMoney/Dollar.cs
@@ -7,18 +7,20 @@ namespace MultiCurrencyMoney
     public class Dollar
     {
         public int amount;
-        //public int amount = 5 * 2;
-        //public int amount = 10;
 
         public Dollar(int amount)
         {
-
+            this.amount = amount;
         }
 
         public void Times(int multiplier)
         {
             // 金額の設定を、コンストラクタからTimesメソッドの中に移動する。
-            amount = 5 * 2;
+            //amount = 5 * 2;
+
+            // 5はどこから来ているかというと、コンストラクタに渡された引数から来ている。
+            // ならば、コンストラクタの中でamountフィールドに代入したらどうだろう。
+            amount *= 2;
         }
     }
 }
