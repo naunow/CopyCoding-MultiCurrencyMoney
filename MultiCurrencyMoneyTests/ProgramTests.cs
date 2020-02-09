7cbe8f73957a46ddf85a1fb6fbc28d8332eb248a
naunow <naunow00@gmail.com>
2020-02-09 21:26:05 +0900

    P.14 Dollarオブジェクトを操作すると、Dollarオブジェクトの値が変化してしまう。（最初のTimesメソッド呼び出しで、5ドルはもう5ドルではなく10ドルになってしまっているから。もしTimesメソッドから新しいオブジェクトが返るなら、元の5ドルに何度でも掛け算ができるし、元の5ドルは変化しなくなる。）

--- MultiCurrencyMoneyTests/ProgramTests.cs
+++ MultiCurrencyMoneyTests/ProgramTests.cs
@@ -15,6 +15,11 @@ namespace MultiCurrencyMoney.Tests
             Dollar five = new Dollar(5);
             five.Times(2);
             Assert.AreEqual(10, five.amount);
+
+            // Dollarオブジェクトを操作すると、Dollarオブジェクトの状態が変化してしまう。
+            // 以下のように書けるようにしたいが、テストは通らない。
+            five.Times(3);
+            Assert.AreEqual(15, five.amount);
         }
     }
 }
