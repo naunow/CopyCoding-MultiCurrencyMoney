f3b2781efae3da65e26525a6f045011fd26e4879
naunow <naunow00@gmail.com>
2020-02-11 17:16:05 +0900

    JavaのassertEqualsメソッドとC#のAssert.AreEqualメソッドは同じインスタンスかどうかまで比較する点が違うので、P.26のテストをC#で記述する場合は書き換える必要がある。

--- MultiCurrencyMoneyTests/ProgramTests.cs
+++ MultiCurrencyMoneyTests/ProgramTests.cs
@@ -16,25 +16,23 @@ namespace MultiCurrencyMoney.Tests
             // だが、ここまで書いてきたテストからは、Dollarを返すことは伝わりにくい。
             // 最初のアサーションを修正して、Dollar同士を直接比較しよう。
 
-            // これで一時変数productは用をなさなくなったので、インライン化(P.258)してしまえる。
             Dollar five = new Dollar(5);
-            //Dollar product = five.Times(2);
-            Assert.AreEqual(new Dollar(10), /*product*/ five.Times(2));
-            //product = five.Times(3);
-            Assert.AreEqual(new Dollar(15), /*product*/ five.Times(3));
+            //Assert.AreEqual(new Dollar(10), five.Times(2));
+            //Assert.AreEqual(new Dollar(15), five.Times(3));
+
+            //『テスト駆動開発』ではJavaで書かれていて上記のテストが通るが、C#だとテストが通らない。
+            // C#のAreEqualメソッドは同じインスタンス同士かまで比較してしまうので、
+            // 以下のように書き換える必要がある。(EqualsメソッドはDollarクラスに自作)
+            Assert.IsTrue(new Dollar(10).Equals(five.Times(2)));
+            Assert.IsTrue(new Dollar(15).Equals(five.Times(3)));
+
         }
 
         [TestMethod()]
         public void TestEquality()
         {
-            // 三角測量を行うためには、実例は2つ必要になる。
-            // すると、等価性比較を一般化する必要が生まれる。
             Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
             Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
-
-            // 実は、Timesメソッドの一般化にも三角測量を使える。
-            // $5 * 2 = $10 と、$5 * 3 = $15 が出てきたら、もうベタ書きの値を返すことはできないからだ。
-
         }
     }
 }
