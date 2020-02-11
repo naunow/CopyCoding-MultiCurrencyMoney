cce0b83300d091b123c4ee4abf60677a263c7ab5
naunow <naunow00@gmail.com>
2020-02-11 17:23:51 +0900

    テストを変更したことで、Dollarクラスのamountフィールドをprivateにできる。 この章の変更によって、私達はあるリスクを受け入れたのだと理解しなければならない。もしも等価性比較のテストが、等価性比較が正確に実装されていることを検証できていなかったのならば、掛け算のテストもきちんと掛け算ができていることを検証できていないということになる。これはTDDが立ち向かうべきリスクだ。
    この章のふりかえり
    ・作成したばかりの機能を使って、テストを改善した。(前章の等価性比較)
    ・そもそも正しく検証できていないテストが2つあったら、もはやお手上げだと気づいた。
    ・そのようなリスクを受け入れて先に進んだ。
    ・テスト対象オブジェクトの新しい機能を使い、テストコードとプロダクトコードの間の結合度を下げた(amountをprivateにした)。
    
    ==TODO==
    ☐ $5 + 10 CHF = $10(レートが2:1の場合)
    ☒ $5 * 2 = $10
    ☒ amountをprivateにする
    ☒ Dollarの副作用をどうする？
    ☐ Moneyの丸め処理どうする？
    ☒ equals()
    ☐ hashcode()
    ☐ nullとの等価性
    ☐ 他のオブジェクトとの等価性比較

--- MultiCurrencyMoney/Dollar.cs
+++ MultiCurrencyMoney/Dollar.cs
@@ -6,7 +6,8 @@ namespace MultiCurrencyMoney
 {
     public class Dollar
     {
-        public int amount;
+        // テストを変更したことで、amountフィールドをprivateに変更できる。
+        private int amount;
 
         public Dollar(int amount)
         {
@@ -15,16 +16,11 @@ namespace MultiCurrencyMoney
 
         public Dollar Times(int multiplier)
         {          
-            // 新しい金額のDollarオブジェクトを返すように変更。
-            // Value Objectパターン(P.238)と呼ばれる。
             return new Dollar(amount * multiplier);
         }
 
         public bool Equals(Object obj)
         {
-            // テストを通すために、trueを返すだけの仮実装を行う。
-            //return true;
-
             Dollar dollar = (Dollar) obj;
             return amount == dollar.amount;
         }
