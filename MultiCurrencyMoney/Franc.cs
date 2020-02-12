7d54457887b25b83ec3d132b7fabb88bd3aa71f9
naunow <naunow00@gmail.com>
2020-02-12 21:32:47 +0900

    P.30 原則をあえて破るとき いよいよ他国通貨の足し算のテストに取り組む。だが、Dollarと同じように動作するFrancオブジェクトを表現する必要がある。 Dollarのテストをコピーして、Francのテストを書いた。 また、Dollarのクラスを丸ごとコピーして、Francのクラスを作成した。 しかし、コピー&ペーストによる再利用は抽象化の敗北であり、きれいな設計を殺すのでは？？ いや、サイクルにはフェーズがあることを思い出そう。 1. テストを書く。 2. コンパイラを通す。 3. テストを走らせ、失敗を確認する。 4. テストを通す。 5. 重複を削除する。
    これで、重複を削除するまで、誰にもコードを見せないはずだ。

---
+++ MultiCurrencyMoney/Franc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiCurrencyMoney
+{
+    public class Franc
+    {
+        private int amount;
+
+        public Franc(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public Franc Times(int multiplier)
+        {
+            return new Franc(amount * multiplier);
+        }
+
+        public bool Equals(Object obj)
+        {
+            Franc franc = (Franc)obj;
+            return amount == franc.amount;
+        }
+
+    }
+}
--- MultiCurrencyMoneyTests/ProgramTests.cs
+++ MultiCurrencyMoneyTests/ProgramTests.cs
@@ -12,17 +12,7 @@ namespace MultiCurrencyMoney.Tests
         [TestMethod()]
         public void TestMultiplication()
         {
-            // 概念的には、DollarのTimesメソッドは、自身の金額とmultiplier引数をかけた値を保持するDollarを返すべきだ。
-            // だが、ここまで書いてきたテストからは、Dollarを返すことは伝わりにくい。
-            // 最初のアサーションを修正して、Dollar同士を直接比較しよう。
-
             Dollar five = new Dollar(5);
-            //Assert.AreEqual(new Dollar(10), five.Times(2));
-            //Assert.AreEqual(new Dollar(15), five.Times(3));
-
-            //『テスト駆動開発』ではJavaで書かれていて上記のテストが通るが、C#だとテストが通らない。
-            // C#のAreEqualメソッドは同じインスタンス同士かまで比較してしまうので、
-            // 以下のように書き換える必要がある。(EqualsメソッドはDollarクラスに自作)
             Assert.IsTrue(new Dollar(10).Equals(five.Times(2)));
             Assert.IsTrue(new Dollar(15).Equals(five.Times(3)));
 
@@ -33,6 +23,14 @@ namespace MultiCurrencyMoney.Tests
         {
             Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
             Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
+        }
+
+        [TestMethod()]
+        public void TestFrancMultiplication()
+        {
+            Franc five = new Franc(5);
+            Assert.IsTrue(new Franc(10).Equals(five.Times(2)));
+            Assert.IsTrue(new Franc(15).Equals(five.Times(3)));
         }
     }
 }
