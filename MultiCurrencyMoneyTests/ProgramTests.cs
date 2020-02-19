abfb0eaa55d1fcce79b85b8638221390bbb5763d
naunow <naunow00@gmail.com>
2020-02-19 21:38:11 +0900

    P.50 残りの箇所についても、ファクトリーメソッドを使うようにテストを書き換えていこう。 利用側のコードからDollarというサブクラスを使っている箇所が消え、この章の最初の時点よりも少しだけ良い状態になった。テストからサブクラスの存在が切り離された結果、既存のコードに影響を及ぼさずに継承構造を変更する権利を手に入れた。

--- MultiCurrencyMoneyTests/ProgramTests.cs
+++ MultiCurrencyMoneyTests/ProgramTests.cs
@@ -16,22 +16,22 @@ namespace MultiCurrencyMoney.Tests
             // そのメソッドはこうやって使う。
             Money five = Money.Dollar(5);
 
-            Assert.IsTrue(new Dollar(10).Equals(five.Times(2)));
-            Assert.IsTrue(new Dollar(15).Equals(five.Times(3)));
+            Assert.IsTrue(Money.Dollar(10).Equals(five.Times(2)));
+            Assert.IsTrue(Money.Dollar(15).Equals(five.Times(3)));
 
         }
 
         [TestMethod()]
         public void TestEquality()
         {
-            Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
-            Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
+            Assert.IsTrue(Money.Dollar(5).Equals(Money.Dollar(5)));
+            Assert.IsFalse(Money.Dollar(5).Equals(Money.Dollar(6)));
 
             Assert.IsTrue(new Franc(5).Equals(new Franc(5)));
             Assert.IsFalse(new Franc(5).Equals(new Franc(6)));
 
             // DollarとFrancを比較したらどうなるのか、テストに書いてみよう。
-            Assert.IsFalse(new Franc(5).Equals(new Dollar(5)));
+            Assert.IsFalse(new Franc(5).Equals(Money.Dollar(5)));
 
         }
 
